a1396cca98850e951b1f0a1305ba13bcb32c0f82
boom <boom@boomy.wtf>
2025-11-12 19:33:08 +1100

    fix: use correct time zone when formatting.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -8,19 +8,19 @@ const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
  * If a timezone is provided, it will format the date accordingly. (Using en-GB locale)
  *
  * @param date string | number - The date to format, can be a string or a number (timestamp).
- * @param timezone string | undefined - The timezone to use for formatting, if provided it will format the date in that timezone.
+ * @param timeZoneIn string | undefined - The timezone to use for formatting, if provided it will format the date in that timezone.
  *                                      However, if this is set the locale will be set to 'en-GB' to ensure consistent formatting.
  *
  * @return string - The formatted date string, or 'Unknown' if the date is falsy.
  */
-export function formatDate(date: string | number | null | undefined, timezone: string | undefined = undefined): string {
+export function formatDate(date: string | number | null | undefined, timeZoneIn: string | undefined = undefined): string {
     if (!date) return 'Unknown';
 
     try {
-        let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+        let expectedTimeZone = userTimeZone;
 
         // If a timezone is provided, use it, or else we want to use the local timezone
-        if (timezone) timeZone = userTimeZone;
+        if (timeZoneIn) expectedTimeZone = timeZoneIn;
 
         const formattedDate = new Date(date)
 
@@ -29,7 +29,7 @@ export function formatDate(date: string | number | null | undefined, timezone: s
             return 'before June 15, 2022';
         }
 
-        let formatter = cachedFormatter.get(timeZone);
+        let formatter = cachedFormatter.get(expectedTimeZone);
         if (!formatter) {
             formatter = new Intl.DateTimeFormat('en-GB', {
                 year: 'numeric',
@@ -39,7 +39,7 @@ export function formatDate(date: string | number | null | undefined, timezone: s
                 minute: 'numeric',
                 hour12: true,
             });
-            cachedFormatter.set(timeZone, formatter);
+            cachedFormatter.set(expectedTimeZone, formatter);
         }
 
         return formatter.format(formattedDate);
