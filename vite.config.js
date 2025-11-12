ea1a98db0ceaa2e4a470c2b0c84e689924c31851
boom <boom@boomy.wtf>
2025-11-12 19:38:26 +1100

    fix: ensure the correct path is used when building via an action.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
 const isProd = process.env.VERCEL_ENV === 'production';
+const isGitHub = process.env.RUN_ENV === 'github';
 
 // Vite configuration
 export default defineConfig({
+    base: isGitHub ? '/tecg/' : '/',
     plugins: [
         react(),
         tailwindcss(),
